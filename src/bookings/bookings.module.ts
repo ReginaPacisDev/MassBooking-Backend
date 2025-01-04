7b8f25f442a31b89cc242623ce24c58ff97ab6f0
ReginaPacisDev <reginapacisdev@gmail.com>
2025-01-04 18:17:55 +0100

    chore: Remove temp file

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -3,12 +3,10 @@ import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
 import { Booking as SequelizeBooking } from '../database';
 import { BOOKINGS_REPOSITORY } from './bookings.type';
-import { TempBookings } from './temp-bookings.service';
 
 @Module({
   providers: [
     BookingsService,
-    TempBookings,
     {
       provide: BOOKINGS_REPOSITORY,
       useValue: SequelizeBooking,
